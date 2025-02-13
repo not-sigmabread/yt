c5ec393c697d3b6f94bc9ed6db63209e129b7bb9
sigma <161086051+sigmabread@users.noreply.github.com>
2025-02-13 16:36:49 -0500

    Delete backend directory

--- backend/models/Playlist.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const PlaylistSchema = new mongoose.Schema({
-  name: String,
-  videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
-});
-
-module.exports = mongoose.model("Playlist", PlaylistSchema);
--- backend/models/Video.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const VideoSchema = new mongoose.Schema({
-  url: String,
-  startTime: Number,
-  endTime: Number,
-});
-
-module.exports = mongoose.model("Video", VideoSchema);
